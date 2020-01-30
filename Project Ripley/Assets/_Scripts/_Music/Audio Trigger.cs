334c0306f4a74e9396a3737e193719b480c9f746
Simon <simonkaks@hotmail.com>
2020-01-30 20:30:06 +0100

    Creating music scene

---
+++ Project Ripley/Assets/_Scripts/_Music/Audio Trigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+
+public class AudioTrigger : MonoBehaviour
+{
+    public StudioEventEmitter musicEmitter;
+
+    public void Detection()
+    {
+
+    }
+}
